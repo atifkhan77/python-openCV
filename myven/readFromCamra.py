ef31eb24a5bc1670ae970d9534ac02590763ba16
atifkhan77 <atifm1190@gmail.com>
2023-09-19 00:49:46 +0500

    added text to video

--- myven/readFromCamra.py
+++ myven/readFromCamra.py
@@ -1,4 +1,5 @@
 import cv2
+import datetime
 cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
@@ -6,13 +7,15 @@ print(cap.isOpened())
 while (cap.isOpened):
     ret, frame  = cap.read()# reading frame
     if ret==True:
-        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) 
-        print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
+        
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        text = 'Width: ' + str(cap.get(3)) + ' Height: '+ str(cap.get(4))# adding text to video 
+        datet = str(datetime.datetime.now()) #adding date and time to video 
+        frame = cv2.putText(frame,datet,(10,50),font,1,(0,255,255),2,cv2.LINE_AA)
         out.write(frame)
-        gray = cv2.cvtColor(frame, cv2.COLOR_Lab2BGR) # changing color of like filters
+       
 
-        cv2.imshow('frame',gray) #creating frame for video which pops up when code is executed
+        cv2.imshow('frame',frame) #creating frame for video which pops up when code is executed
 
         if cv2.waitKey(1) & 0xFF == ord('q'):# frame will close when 'q' is pressed 
             break
