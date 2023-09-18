756dc995ec36a26bcc57e6e0abdb2a402080dde6
atifkhan77 <atifm1190@gmail.com>
2023-09-18 21:26:37 +0500

    adding comments for better understanding

--- myven/readFromCamra.py
+++ myven/readFromCamra.py
@@ -4,15 +4,17 @@ fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 print(cap.isOpened())
 while (cap.isOpened):
-    ret, frame  = cap.read()
+    ret, frame  = cap.read()# reading frame
     if ret==True:
-        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) 
         print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
         out.write(frame)
-        gray = cv2.cvtColor(frame, cv2.COLOR_Lab2BGR)
-        cv2.imshow('frame',gray)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        gray = cv2.cvtColor(frame, cv2.COLOR_Lab2BGR) # changing color of like filters
+
+        cv2.imshow('frame',gray) #creating frame for video which pops up when code is executed
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):# frame will close when 'q' is pressed 
             break
     else:
         break
